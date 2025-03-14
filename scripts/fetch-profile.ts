2d10ddc6be37b989234147593d456dbba93e183a
The SunShining <thesunshiningmin@gmail.com>
2025-03-14 16:43:35 -0600

    Update fetch-profile.ts

--- scripts/fetch-profile.ts
+++ scripts/fetch-profile.ts
@@ -4,7 +4,7 @@ const ACCOUNT_ID = 'sleet.near';
 
 async function main() {
   const nearSocial = new Social({
-    network: 'mainnet',
+    networkId: 'mainnet',
     nodeUrl: 'https://rpc.mainnet.near.org',
   });
 
@@ -15,9 +15,9 @@ async function main() {
     console.log(JSON.stringify(profile, null, 2));
 
     // Fetch social links
-    const links = await nearSocial.get({
-      keys: [`${ACCOUNT_ID}/profile/links/**`]
-    });
+    const links = await nearSocial.get(
+      `${ACCOUNT_ID}/profile/links/**`
+    );
     console.log('\nSocial Links:');
     console.log(JSON.stringify(links, null, 2));
 
