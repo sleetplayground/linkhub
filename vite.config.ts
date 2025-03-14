86e463fcab5135242081e408752112e681b83ea7
The SunShining <thesunshiningmin@gmail.com>
2025-03-14 17:19:14 -0600

    ug

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,21 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'process.env': process.env,
+    global: {}
+  },
+  resolve: {
+    alias: {
+      buffer: 'buffer',
+      process: 'process/browser'
+    }
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: 'globalThis'
+      }
+    }
+  }
 })
