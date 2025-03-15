49b8ba5360124ecddeb8fdada0e80d9893eedbb6
The SunShining <thesunshiningmin@gmail.com>
2025-03-14 19:10:59 -0600

    /// HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route, useNavigate } from 'react-router-dom'
 import { useState } from 'react'
 import ProfilePage from './pages/ProfilePage'
 
